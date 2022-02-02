575e2d57914678c459b87b37186d52015f87a22d
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2022-02-02 14:40:40 -0300

    Refatoração HTML

---
+++ js/data.js
@@ -0,0 +1,30 @@
+const projects = [
+  {
+    name: 'To Do List',
+    image: null,
+    description: 'Este projeto é uma aplicação web que tem como objetivo auxiliar no gerenciamento de atividades. Nele podemos registrar as tarefas que devemos realizar, excluí-las, marcá-las como concluídas, e salvá-las no nosso navegador.',
+    skills: ['HTML', 'CSS', 'Javascript', 'Git', 'GitHub'],
+    link: './projetos/todo-list',
+  },
+  {
+    name: 'Pixels Art',
+    image: './img/pixels-art.png',
+    description: 'Este projeto é uma aplicação web que tem como objetivo ser um entretenimento para o usuário. Nele podemos usar a imaginação para desenhar pintando os quadrados com as cores geradas randomicamente.',
+    skills: ['HTML', 'CSS', 'Javascript', 'Git', 'GitHub'],
+    link: '/projetos/pixels-art',
+  },
+  {
+    name: 'Project ShoppingCart',
+    image: './img/shopping-cart.png',
+    description: 'Este projeto é uma aplicação web que tem como objetivo simular um carrinho de compras em um e-commerce. Nele podemos pesquisar por produtos fornecidos pela API do Mercado Livre e adicionar esses produtos ao carrinho de compras, que os armazena e soma o valor total da compra.',
+    skills: ['HTML', 'CSS', 'Javascript', 'API', 'Git', 'GitHub'],
+    link: '/projetos/shooping-cart/',
+  },
+  {
+    name: 'E-commerce (Front-end)',
+    image: null,
+    description: 'Este projeto é uma aplicação web desenvolvida em React que tem como objetivo simular um e-commerce básico. Nele podemos pesquisar por produtos fornecidos pela API do Mercado Livre pelo nome e/ou categoria, ver uma descrição detalhadas de cada produto em página de detalhes, adicionar ao carrinho de compras, alterar a quantidade de produtos no carrinho e/ou removê-lo do mesmo e acessar uma página para finalizar a compra, em que podemos ver um resumo das aquisições e inserir os dados para compra, que não é disponilizada neste projeto.',
+    skills: ['HTML', 'CSS', 'Javascript', 'React', 'API', 'Git', 'GitHub'],
+    link: 'https://angry-hodgkin-648f7f.netlify.app/',
+  }
+];
---
+++ js/index.js
@@ -0,0 +1,57 @@
+const projectsContainer = document.getElementById('projects');
+
+const createElement = (tag, property = false, value = false) => {
+  const element = document.createElement(tag);
+  if (property) element[property] = value;
+  return element;
+}
+
+projects.forEach((project) => {
+  const { image, name, description, link } = project;
+
+  const projectContainer = createElement('section', 'className', 'project');
+
+  if (image !== null) {
+    const img = createElement('img', 'src', image);
+    projectContainer.appendChild(img);
+  } else {
+    const h4 = createElement('h4', 'innerHTML', name);
+    projectContainer.appendChild(h4);
+  }
+
+  const projectDataContainer = createElement('div', 'className', 'project-data');
+
+  const descriptionContainer = createElement('div', 'className', 'description');
+
+  const h5 = createElement('h5', 'innerHTML', 'Descrição');
+
+  const p = createElement('p', 'innerHTML', description);
+
+  descriptionContainer.appendChild(h5);
+  descriptionContainer.appendChild(p);
+
+  projectDataContainer.appendChild(descriptionContainer);
+
+  const skillsContainer = createElement('section', 'className', 'skills');
+
+  const h5Skills = createElement('h5', 'innerHTML', 'Tecnologias utilizadas neste projeto');
+
+  const ul = createElement('ul');
+
+  project.skills.forEach((skill) => {
+    const li = createElement('li', 'innerHTML', skill);
+    ul.appendChild(li);
+  });
+
+  skillsContainer.appendChild(h5Skills);
+  skillsContainer.appendChild(ul);
+  projectDataContainer.appendChild(skillsContainer);
+
+  const a = createElement('a', 'href', link);
+  a.innerHTML = "Clique aqui para acessar este projeto"
+
+  projectDataContainer.appendChild(a);
+  projectContainer.appendChild(projectDataContainer);
+  projectsContainer.appendChild(projectContainer);
+});
+
